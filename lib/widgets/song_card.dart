4a4c72f60c0eb8b84f61eaa7a5c73b36d6e3f5cb
Nnamdi Chidera Emmanuel <69612512+Dera-codes@users.noreply.github.com>
2023-10-05 19:36:38 +0100

    app update

--- lib/widgets/song_card.dart
+++ lib/widgets/song_card.dart
@@ -15,6 +15,7 @@ class SongCard extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(right: 10),
       child: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
           Container(
             width: MediaQuery.of(context).size.width * 0.45,
@@ -32,12 +33,36 @@ class SongCard extends StatelessWidget {
             width: MediaQuery.of(context).size.width * 0.45,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15.0),
-              image: DecorationImage(
-                image: AssetImage(
-                  song.coverUrl,
+              color: Colors.white.withOpacity(0.8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      song.title,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    Text(
+                      song.description,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ],
                 ),
-                fit: BoxFit.cover,
-              ),
+                const Icon(
+                  Icons.play_circle,
+                  color: Colors.deepPurple,
+                ),
+              ],
             ),
           ),
         ],
